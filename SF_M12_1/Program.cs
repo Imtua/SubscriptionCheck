ccfc2270c91d241a5f41109725f6cc6053c51435
Imtua <itsololevelling@gmail.com>
2023-04-15 19:29:22 +0300

    Добавьте файлы проекта.

---
+++ SF_M12_1/Program.cs
@@ -0,0 +1,14 @@
+﻿using SF_M12_1;
+
+SubscriptionCheck(new User("TestLogin", "Дмитрий", true));
+SubscriptionCheck(new User("Test11", "Владимир", false));
+
+static void SubscriptionCheck(User? user)
+{
+    Console.WriteLine("Добрый день, {0}!!", user.Name);
+    if (!user.IsPremium)
+	{
+		User.ShowAds();
+	}
+    Console.WriteLine("До свидания, {0}. Удачи!!", user.Name);
+}
---
+++ SF_M12_1/User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace SF_M12_1
+{
+    internal class User
+    {
+        public string Login { get; set; }
+        public string Name { get; set; }
+        public bool IsPremium { get; set; }
+        public User() { }
+        public User(string login, string name, bool subscription)
+        {
+            Login = login;
+            Name = name;
+            IsPremium = subscription;
+        }
+
+        public static void ShowAds()
+        {
+            Console.WriteLine("Посетите наш новый сайт с бесплатными играми free.games.for.a.fool.com");
+            Thread.Sleep(1000);
+
+            Console.WriteLine("Купите подписку на МыКомбо и слушайте музыку везде и всегда");
+            Thread.Sleep(2000);
+
+            Console.WriteLine("Оформите премиум подписку на наш сервис, чтобы не видеть рекламу!");
+            Thread.Sleep(3000);
+        }
+    }
+}
